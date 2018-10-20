839faca89fd7ea723d14ff75df5b16775e1a9ccd
stellasylee <leeseoye@grinnell.edu>
2018-10-20 03:33:50 -0500

    not fully functioning notification

--- app/src/main/java/com/example/leeseoye/shelfr/MainActivity.java
+++ app/src/main/java/com/example/leeseoye/shelfr/MainActivity.java
@@ -1,13 +1,45 @@
 package com.example.leeseoye.shelfr;
 
+import android.app.AlarmManager;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.Calendar;
+import java.util.TimeZone;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NotificationReceiver Receiver = new NotificationReceiver();
+    private static final String ACTION_NOTIFICATION = "com.leeseoye.shelfr.ACTION_NOTIFICATION";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        registerReceiver(Receiver, new IntentFilter(ACTION_NOTIFICATION));
+
+        AlarmManager alarm = (AlarmManager) this.getSystemService(this.ALARM_SERVICE);
+        Intent intent = new Intent(this, NotificationManager.class);
+        PendingIntent alarmIntent = PendingIntent.getBroadcast(this, 0, intent, 0);
+        Log.d("setalarm", "here");
+        Calendar alarmTime = Calendar.getInstance();
+        alarmTime.setTimeZone(TimeZone.getTimeZone("America/Chicago"));
+        alarmTime.set(2018,10,19,2,12);
+        Log.d("setalarm", alarmTime.toString() );
+        alarm.set(alarm.RTC_WAKEUP, alarmTime.getTimeInMillis(), alarmIntent);
+
+        Toast.makeText(this.getApplicationContext(), "absdfagthyjuyukikjhgf", Toast.LENGTH_LONG).show();
     }
+
+
+
+
+
 }
--- app/src/main/java/com/example/leeseoye/shelfr/NotificationReceiver.java
+++ app/src/main/java/com/example/leeseoye/shelfr/NotificationReceiver.java
@@ -1,12 +1,18 @@
 package com.example.leeseoye.shelfr;
 
-
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+import android.widget.Toast;
+
+import static android.content.Context.NOTIFICATION_SERVICE;
 
 public class NotificationReceiver extends BroadcastReceiver {
     //private NotificationManager notificationManager;
@@ -16,14 +22,44 @@ public class NotificationReceiver extends BroadcastReceiver {
     // Notification ID.
     private static final int NOTIFICATION_ID = 0;
     // Notification channel ID.
-    private static final String CHANNELL_ID = "notification_channel";
+    private static final String CHANNEL_ID = "notification_channel";
+    private Context context;
+    private static final String ACTION_NOTIFICATION = "com.leeseoye.shelfr.ACTION_NOTIFICATION";
 
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        Log.d("tag","receive");
+        this.context = context;
+        createNotificationChannel();
+        notificationManager = (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
         // send notification
         sendNotification(context);
+
+    }
+
+    /**
+     * OnClick method for the "Cancel Me!" button. Cancels the notification.
+     */
+    public void cancelNotification() {
+        // Cancel the notification.
+        //TODO change the NOTIFICATIONID to unique for each alarm
+        notificationManager.cancel(NOTIFICATION_ID);
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only API 26+ (no supporting library for other API)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            //CharSequence name = getString(R.string.channel_name);
+            //String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "name", importance);
+            channel.setDescription("description");
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = context.getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 
     /**
@@ -32,18 +68,19 @@ public class NotificationReceiver extends BroadcastReceiver {
      * @param context, activity context.
      */
     private void sendNotification(Context context) {
-        Intent contentIntent = new Intent(context, MainActivity.class);
-
+       // Intent contentIntent = new Intent(context, MainActivity.class);
+        Intent contentIntent = new Intent(ACTION_NOTIFICATION);
         PendingIntent contentPendingIntent = PendingIntent.getActivity
                 (context, NOTIFICATION_ID, contentIntent, PendingIntent
                         .FLAG_UPDATE_CURRENT);
         // Build the notification
+        //TODO: change the contentTitle and contentText
         NotificationCompat.Builder builder = new NotificationCompat.Builder
-                (context, CHANNELL_ID)
+                (context, CHANNEL_ID)
                 .setSmallIcon(R.drawable.fridge)
                 .setContentTitle("title")
                 .setContentText("text")
-                .setContentIntent(contentPendingIntent)
+                //.setContentIntent(contentPendingIntent)
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT)
                 .setAutoCancel(true)
                 .setDefaults(NotificationCompat.DEFAULT_ALL);
@@ -51,4 +88,5 @@ public class NotificationReceiver extends BroadcastReceiver {
         // Notify the notification
         notificationManager.notify(NOTIFICATION_ID, builder.build());
     }
+
 }
