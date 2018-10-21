69f9263c2c0d7fba000f67725070c0eaa48267a6
stellasylee <leeseoye@grinnell.edu>
2018-10-21 03:25:56 -0500

    notification set

--- app/src/main/java/com/example/leeseoye/shelfr/NotificationReceiver.java
+++ app/src/main/java/com/example/leeseoye/shelfr/NotificationReceiver.java
@@ -9,7 +9,6 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
-import android.widget.Toast;
 
 import static android.content.Context.NOTIFICATION_SERVICE;
 
